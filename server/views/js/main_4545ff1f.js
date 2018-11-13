046122db2464e223eee03a139808f8a72f2a1b06
fuyunfeng <ex-fuyunfeng@xianglin.cn>
2018-11-13 16:08:09 +0800

    每次编译  先删除server端views

--- server/views/js/main_4545ff1f.js
+++ server/views/js/main_4545ff1f.js
@@ -51965,7 +51965,7 @@ _reactDom2.default.render(_react2.default.createElement(
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var socket = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/lib/index.js")('http://localhost:9093'); //{'reconnect':false,'auto connect':false}
+var socket = __webpack_require__(/*! socket.io-client */ "./node_modules/socket.io-client/lib/index.js")('http://http://120.79.220.248:9093'); //{'reconnect':false,'auto connect':false}
 
 // 获取用户登录后  后端返回监听
 var loginObject = {
@@ -52085,4 +52085,4 @@ exports.gameOverObject = gameOverObject;
 /***/ })
 
 /******/ });
-//# sourceMappingURL=main_958dc11e.js.map
+//# sourceMappingURL=main_4545ff1f.js.map
